3c92bf6593dbf428f570ec973502ffe3cddcc7c7
OrAmram100 <or.amram@s.afeka.ac.il>
2021-02-18 21:41:22 +0200

    //update

--- FinalProject/src/model/Product.java
+++ FinalProject/src/model/Product.java
@@ -1,10 +1,17 @@
 package model;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class Product {
 	private String productName;
 	private int PriceToStore;
 	private int PriceToCostumer;
 	private Customer customer;
+	private final static int MAX_NAME_SIZE = 20;
+	public static final int PRODUCT_SIZE = (MAX_NAME_SIZE*2) + Customer.CUSTOMER_SIZE + 4 + 4;
+
 
 	public Product(String productName, int castToStore, int costToCostumer, Customer customer) {
 		this.productName = productName;
@@ -45,6 +52,21 @@ public class Product {
 
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
+	}
+	public static Product readProductFromFile(DataInput dIn) throws IOException
+	{
+		String name = binFile.readStringFromFile(MAX_NAME_SIZE, dIn);
+		int sellingPrice = dIn.readInt();
+		int buyingPrice = dIn.readInt();
+		Customer c = Customer.readCustomerToFile(dIn);
+		return new Product(name, buyingPrice, sellingPrice, c);
+	}
+	public void writeProductToFile(DataOutput dOut) throws IOException
+	{
+		binFile.writeStringToFile(productName, MAX_NAME_SIZE, dOut);
+		dOut.writeInt(PriceToCostumer);
+		dOut.writeInt(PriceToStore);
+		customer.writeCustomerToFile(dOut);
 	}
 
 	@Override
